4480566d2c05d639903dd06322e8199681f65652
Ben Silverman <bensilverman10@gmail.com>
2025-01-04 14:36:08 +0000

    add basic DB

---
+++ app/db.server/schema.ts
@@ -0,0 +1,19 @@
+import { randomUUID } from "crypto";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const inputs = sqliteTable("inputs", {
+	id: text().$defaultFn(randomUUID).primaryKey(),
+	time: integer().notNull(),
+	award_count: integer().notNull(),
+	data: text().notNull(),
+	uploaded_at: integer({ mode: "timestamp_ms" }),
+	track_id: text().notNull(),
+	author_id: text()
+		.notNull()
+		.references(() => authors.id),
+});
+
+export const authors = sqliteTable("authors", {
+	id: text().$defaultFn(randomUUID).primaryKey(),
+	username: text().notNull(),
+});
